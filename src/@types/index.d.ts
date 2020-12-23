1ff710268bc9628c0efb926cd212d4ea1717d3c6
Vasily Novosad <delhsmail@gmail.com>
2020-12-23 11:58:02 +0500

    Documentation

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -43,10 +43,13 @@ declare module '@via-profit-services/knex' {
    * Key - is a alias name \
    * Value - is a field alias name or array of names \
    * Use asterisk (\*) for default alias name. \
-   * For example: {\
-   * books: ['title', 'length'],\
-   * info: ['*'],\
+   * For example:
+   * ```js
+   * {
+   *   books: ['title', 'length'],
+   *   info: ['*'],
    * }
+   * ```
    */
   export type TableAliases = {
     [key: string]: string | string[];
@@ -85,9 +88,9 @@ declare module '@via-profit-services/knex' {
      * Default: `true`
      */
     enablePgTypes?: boolean;
-    migrations?: Knex.MigratorConfig;
-    seeds?: Knex.SeederConfig;
-    pool?: Knex.PoolConfig;
+    migrations?: Omit<Knex.MigratorConfig, 'database'>;
+    seeds?: Omit<Knex.SeederConfig, 'variables'>;
+    pool?: Omit<Knex.PoolConfig, 'name' | 'log' | 'refreshIdle' | 'returnToHead' | 'priorityRange'>;
   }
 
 
@@ -108,7 +111,7 @@ declare module '@via-profit-services/knex' {
 
 
   export type ConvertOrderByToKnex = (orderBy: OrderBy | undefined) => OrderByKnex;
-  export type ConvertJsonToKnex = <TRecord = any>(knex: Knex, json: any | any[]) => Knex.Raw<TRecord>;
+  export type ConvertJsonToKnex = <TRecord = unknown>(knex: Knex, data: unknown | string) => Knex.Raw<TRecord>;
   export type ConvertBetweenToKnex = (
     builder: Knex.QueryBuilder,
     between: Between | undefined,
@@ -137,20 +140,119 @@ declare module '@via-profit-services/knex' {
   }
 
   /**
-   * Convert ObderBy array to Knex Order by format
+   * Convert Order array to Knex Order by format\
+   * Example:
+   * ```ts
+   * const orderBy = [{
+   *   field: 'name',
+   *   direction: 'asc',
+   * }];
+   * 
+   * await knex
+   *   .select(['*'])
+   *   .from('books')
+   *   .orderBy(convertOrderByToKnex(orderBy)); // <-- [{ column: 'name', order: 'asc' }]
+   * 
+   * ```
    */
   export const convertOrderByToKnex: ConvertOrderByToKnex;
 
   /**
-   * Convert JSON string to Knex Raw 
+   * Convert object or JSON string to Knex Raw
    */
   export const convertJsonToKnex: ConvertJsonToKnex;
+
+  /**
+   * Convert Between data to knex builder data\
+   * Example:
+   * ```ts
+   * const between = {
+   *   pages: {
+   *     start: 300,
+   *     end: 800,
+   *   }
+   * };
+   * 
+   * await knex
+   *   .select(['*'])
+   *   .from('books')
+   *   .where((builder) => convertBetweenToKnex(builder, between));
+   * 
+   * ```
+   * In third argument you can passed options:
+   *  - **timezone** `string`. used with operations between dates
+   *  - **aliases** `TableAliases`. See `ApplyAliases`.
+   */
   export const convertBetweenToKnex: ConvertBetweenToKnex;
+
+  /**
+   * Convert Where clause to knex builder\
+   * Example:
+   * ```ts
+   * const where = [
+   *   ['year', '=', 1992],
+   *   ['pages', '>', 30],
+   * ];
+   * 
+   * await knex
+   *   .select(['*'])
+   *   .from('books')
+   *   .where((builder) => convertWhereToKnex(builder, where));
+   * 
+   * ```
+   */
   export const convertWhereToKnex: ConvertWhereToKnex;
+
+  /**
+   * Convert `OutputSearch` to knex builder\
+   * Example:
+   * ```ts
+   * const search = [{
+   *   field: 'title',
+   *   query: 'kitchen',
+   * }];
+   * 
+   * await knex
+   *   .select(['*'])
+   *   .from('books')
+   *   .where((builder) => convertSearchToKnex(builder, search));
+   * 
+   * ```
+   */
   export const convertSearchToKnex: ConvertSearchToKnex;
   
   /**
    * Apply aliases map to where clause array
+   * 
+   * ```ts
+   * const where = [
+   *   ['year', '=', 1992],
+   *   ['pages', '>', 30],
+   *   ['author', 'is not null'],
+   * ];
+   * 
+   * const aliases = {
+   *   books: ['year', 'pages'],
+   *   author: ['author'],
+   * }
+   * const where = applyAliases();
+   * ```
+   * 
+   * You can use asterisk (`*`) for default alias name:
+   * 
+   * ```ts
+   * const where = [
+   *   ['year', '=', 1992],
+   *   ['pages', '>', 30],
+   *   ['author', 'is not null'],
+   * ];
+   * 
+   * const aliases = {
+   *   books: [*],
+   *   author: ['author'],
+   * }
+   * const where = applyAliases();
+   * ```
    */
   export const applyAliases: ApplyAliases;
 
@@ -176,9 +278,9 @@ declare module '@via-profit-services/knex' {
    */
   export const extractTotalCountPropOfNode: ExtractTotalCountPropOfNode;
 
-  export const DATABASE_CHARSET: string;
-  export const DATABASE_CLIENT: string;
-  export const DEFAULT_TIMEZONE: string;
+  export const DATABASE_CHARSET: 'UTF8';
+  export const DATABASE_CLIENT: 'pg';
+  export const DEFAULT_TIMEZONE: 'UTC';
 
   export const factory: KnexGraphqlMiddlewareFactory;
 
